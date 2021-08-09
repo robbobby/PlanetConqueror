37e168b14545de6858be531cf14d840915b6ac44
robbobby <69080884+robbobby@users.noreply.github.com>
2021-08-09 15:25:10 +0100

    Added gitignore

---
+++ PlanetConquerorCore/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace PlanetConquerorCore {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
