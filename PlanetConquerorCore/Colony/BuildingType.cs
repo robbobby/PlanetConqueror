f13744fa7031f6d66fceb3dd54b9d9b124f99b93
robbobby <69080884+robbobby@users.noreply.github.com>
2021-08-09 16:39:54 +0100

    Added more base classes

---
+++ PlanetConquerorCore/Colony/BuildingType.cs
@@ -0,0 +1,49 @@
+namespace PlanetConquerorCore.Colony {
+    public enum BuildingType {
+        // Housing
+        Apartment,
+        CommunalHouse,
+        HabitatT1,
+        HabitatT2,
+        HabitatT3,
+        SkyscraperT1,
+        SkyscraperT2,
+        
+        // Resource Storage
+        MiniStorage,
+        StorageDepotT1,
+        StorageDepotT2,
+        StorageDepotT3,
+        
+        // Resources
+        StoneMine,
+        WaterWell,
+        WaterPump,
+        WaterTreatmentPlant,
+        
+        GreenHouseT1,
+        GreenHouseT2,
+        GreenHouseT3,
+        FarmT1,
+        FarmT2,
+        FarmT3,
+        
+        // ResourceUsage
+        Generator,
+        PowerPlant,
+        Battery,
+        LightningTower,
+        SolarPanel,
+        WindTurbine,
+        
+        // Oxygen
+        AirFilter,
+        IntakeFan,
+        OxygenGenerator,
+        AirScrubber,
+        
+        // LandingPad
+        RocketLandingPad,
+        
+    }
+}
--- PlanetConquerorCore/Colony/Colony.cs
+++ PlanetConquerorCore/Colony/Colony.cs
@@ -8,7 +8,16 @@ namespace PlanetConquerorCore.Colony {
         public List<Resource> ResourcesCollected { get; private set; }
         public List<Colonist> People { get; private set; } = new List<Colonist>();
         public List<Building> Buildings { get; private set; } = new List<Building>();
+        
+        public Colony(string _name, int _money) {
+            Name = _name;
+            Money = _money;
+            InitResourceList();
+        }
+        private void InitResourceList() {
+            ResourcesCollected = new List<Resource>();
+            // TODO: loop through enum of resource type and make initial ResourcesCollected list
+        }
     }
     
-    // TODO: loop through enum of resource type and add to ResourcesCollected list
 }
